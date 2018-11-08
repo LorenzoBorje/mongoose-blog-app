2ba813eaa88f9d55e4454d6e5d6a154554b0199e
Lorenzo Borje <lorenzo.borje@gmail.com>
2018-11-08 20:56:18 +0800

    completed PUT endpoints

--- server.js
+++ server.js
@@ -65,11 +65,33 @@ app.post('/blog-posts', (req, res) => {
 });
 
 app.put('/blog-posts/:id', (req, res) => {
+
   if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
     const message = 
      `Request path id ${req.params.id} and request body id ${req.body.id} must match`;
     console.error(message);
+    return res.status(400).send(message);
   }
+
+  const toUpdate = {}
+  const updatableFields = ['title', 'content', 'author'];
+
+  updatableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+  
+  
+  Posts
+    .findByIdAndUpdate(req.body.id, {$set: toUpdate})
+    .then(post => res.status(200).json(post.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Internal server error'});
+    });  
+
+
 });
 
 function runServer(databaseUrl, port = PORT) {
