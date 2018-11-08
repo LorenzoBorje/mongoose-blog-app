044aa9ee0ccad678ab367f7252d9bdaf691b7f65
Lorenzo Borje <lorenzo.borje@gmail.com>
2018-11-08 20:37:38 +0800

    completed POST endpoint

--- server.js
+++ server.js
@@ -37,6 +37,41 @@ app.get('/blog-posts/:id', (req, res) => {
     });
 });
 
+app.post('/blog-posts', (req, res) => {
+  
+  const requiredFields = ['title', 'content', 'author'];
+  requiredFields.forEach(field => {
+    if (!(field in req.body)) {
+      const message = `Missing ${field} in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  });
+  
+  Posts.create({
+    title: req.body.title,
+    content: req.body.content,
+    author: {
+      firstName: req.body.author.firstName,
+      lastName: req.body.author.lastName
+    }
+  })
+    .then(post => res.status(201).json(post.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: "Internal server error"});
+    });
+
+});
+
+app.put('/blog-posts/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = 
+     `Request path id ${req.params.id} and request body id ${req.body.id} must match`;
+    console.error(message);
+  }
+});
+
 function runServer(databaseUrl, port = PORT) {
   return new Promise((resolve, reject) => {
     mongoose.connect(
