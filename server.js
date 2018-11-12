b1faba3440765a73ec4f62b074b9c3cfc64a2823
Lorenzo Borje <lorenzo.borje@gmail.com>
2018-11-12 19:50:37 +0800

    added PUT /authors endpoint

--- server.js
+++ server.js
@@ -57,12 +57,49 @@ app.post('/authors', (req, res) => {
       console.error(err);
       res.status(500).json({ error: 'Internal server error' });
     });
-
-
-  
-  
-
 });
+
+app.put('/authors/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = `Request path id ${req.params.id} and request body id ${req.body.id} must match`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+
+  const toUpdate = {};
+  const updatableFields = ['firstName', 'lastName', 'userName'];
+
+  updatableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+  Authors
+    .findOne({userName: req.body.userName})
+    .then(author => {
+      if (author) {
+        const message = `User name ${req.body.userName} is taken. Try another?`
+        console.error(message);
+        return res.status(400).send(message);
+      } else {
+        Authors
+          .findByIdAndUpdate(req.body.id, {$set: toUpdate}, {new: true})
+          .then(author => {
+            res.status(200).json({
+              id: author.id,
+              name: `${author.firstName} ${author.lastName}`,
+              userName: author.userName
+            })
+          })
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Internal server error'});
+    });
+;
+
+})
 
 app.get('/blog-posts', (req, res) => {
   BlogPosts.find()
@@ -144,7 +181,7 @@ app.put('/blog-posts/:id', (req, res) => {
   
   
   BlogPosts
-    .findByIdAndUpdate(req.body.id, {$set: toUpdate})
+    .findByIdAndUpdate(req.body.id, {$set: toUpdate},  {new: true})
     .then(post => res.status(200).json(post.serialize()))
     .catch(err => {
       console.error(err);
